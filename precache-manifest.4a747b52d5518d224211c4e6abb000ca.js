c4f09f1841edab276a967f2978077851d181f57a
Andrii <andriibozhko1@gmail.com>
2019-02-25 18:19:44 +0200

    to gh-pages

---
+++ precache-manifest.4a747b52d5518d224211c4e6abb000ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fff847a5a19626bf9aa",
+    "url": "/react-data-table/static/css/main.f7867d55.chunk.css"
+  },
+  {
+    "revision": "0fff847a5a19626bf9aa",
+    "url": "/react-data-table/static/js/main.0fff847a.chunk.js"
+  },
+  {
+    "revision": "6e9c37121d91e30ae558",
+    "url": "/react-data-table/static/js/runtime~main.6e9c3712.js"
+  },
+  {
+    "revision": "21a7517dc8768f362c79",
+    "url": "/react-data-table/static/js/2.21a7517d.chunk.js"
+  },
+  {
+    "revision": "c4cf35152618b38ee65711818a582df5",
+    "url": "/react-data-table/index.html"
+  }
+];
